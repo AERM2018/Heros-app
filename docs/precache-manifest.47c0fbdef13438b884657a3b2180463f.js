e9c613427fd76ea2829e5332ecd056955ddce2db
Angel Retana <retana.martinez.angel.edaurdo@gmail.com>
2020-09-09 18:23:59 -0500

    Agregando la carpeta docs

---
+++ docs/precache-manifest.47c0fbdef13438b884657a3b2180463f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cc59ed3233ba6b1a454c169fb1c5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e233250a7580ab6f10d3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a44f09644a0144a323da",
+    "url": "/static/js/2.ce0dac07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce0dac07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e233250a7580ab6f10d3",
+    "url": "/static/js/main.da3886d9.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
